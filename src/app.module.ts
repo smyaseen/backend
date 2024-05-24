422ac02634df3e9f41ba2f8b94ba7a9906e631cc
smyaseen <smyaseen164@gmail.com>
2024-05-24 22:25:10 +0500

    feat: added throttling to mitigate attacks like DDOS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { PrismaService } from './prisma.service';
 import { EnvironmentConfigModule } from './config/environment-config/environment-config.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [AuthModule, UsersModule, EnvironmentConfigModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    AuthModule,
+    UsersModule,
+    EnvironmentConfigModule,
+  ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
